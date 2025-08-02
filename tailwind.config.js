d2fb021f74136a9728ea1a103e20a69a89a840b9
Anniversary-AH <eken044@gmail.com>
2025-08-02 16:16:22 +1000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'wow-gold': '#ffd700',
+        'wow-blue': '#0070dd',
+        'wow-purple': '#a335ee',
+        'wow-orange': '#ff8000',
+        'wow-green': '#1eff00',
+      }
+    },
+  },
+  plugins: [],
+}
